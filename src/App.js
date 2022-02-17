697adbe05808d8c823c0539748f8125c099e21c5
asd <asd@gmail.com>
2022-02-17 13:35:47 +0700

    react-json

--- src/App.js
+++ src/App.js
@@ -1,76 +1,32 @@
-import React, { Component } from 'react'
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import axios from 'axios';
-const api = axios.create({
-  baseURL: `http://localhost:3000/posts`
-})
-class App extends Component {
-  state = {
-    users: []
-  }
-  componentDidMount() {
-    this.getUsers();
-  }
-  getUsers = async () => {
-    let items = await api.get('/').then(({ data }) => data);
-    this.setState({ users: items })
-  }
-  createUser = async () => {
-    await api.post('/', { id: 18, name: "json-z", author: "z" })
-      .then(({ data }) => data)
-    this.getUsers();
-  }
-  updateUser = async (id, data) => {
-    await api.patch(`/${id}`, { name: data })
-    this.getUsers();
-  }
-  deleteUser = async (id) => {
-    await api.delete(`/${id}`)
-    this.getUsers();
-  }
+import { DataGrid } from '@mui/x-data-grid';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
-  render() {
+
+const DataTable = () => {
+    const data = useState([])
+    axios.get('http://localhost:3000/posts')
+        .then(({ data }) => console.log(data))
+    const rows = { data }
+
+
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 70 },
+        { field: 'name', headerName: 'Name', width: 130 },
+        { field: 'email', headerName: 'Email', width: 130 },
+        { field: 'author', headerName: 'Author', width: 130 },
+    ];
     return (
-      <Container component="main">
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Button sx={{ m: 2, p: 2 }} color="secondary" variant="contained" onClick={() => this.createUser()}>Create</Button>
-          <table className="">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Author</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.users.map(user =>
-                <tr key={user.id}>
-                  <td sx={{ m: 2, p: 2 }}>{user.name}</td>
-                  <td sx={{ m: 2, p: 2 }}>{user.author}</td>
-                  <td sx={{
-                    display: 'flex',
-                    justifyContent:'flex-end',
-                  }} >
-                    <Button sx={{ m: 2, p: 2 }} color="secondary" variant="contained" onClick={() => this.updateUser(user.id, `${user.name}EditedName`)}>Edit</Button>
-                    <Button sx={{ m: 2, p: 2 }} color="secondary" variant="contained" onClick={() => this.deleteUser(user.id)}>Delete</Button>
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        </Box>
-      </Container >
+        <div style={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSize={5}
+                rowsPerPageOptions={[5]}
+                checkboxSelection
+            />
+        </div>
     );
-  }
 }
 
-export default App;
+export default DataTable
