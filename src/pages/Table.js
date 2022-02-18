128e565910f47c5f88d94f64d1f5659ef95faffb
asd <asd@gmail.com>
2022-02-18 07:08:52 +0700

    react-json

---
+++ src/pages/Table.js
@@ -0,0 +1,103 @@
+import { DataGrid } from '@mui/x-data-grid';
+import React, { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import axios from 'axios'
+
+
+const Table = () => {
+
+    const [tableData, getTableData] = useState([])
+
+    const [open, setOpen] = useState(false);
+
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/posts')
+            .then(response => {
+                getTableData(response.data)
+            })
+    }, [])
+
+    const handleClick = (cellValues) => {
+
+    };
+
+    const handleCellClick = (param, event) => {
+        event.stopPropagation();
+    };
+
+    const handleRowClick = (param, event) => {
+        event.stopPropagation();
+    };
+
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 70 },
+        { field: 'name', headerName: 'Name', width: 130 },
+        { field: 'author', headerName: 'Author', width: 130 },
+        {
+            field: 'action', headerName: 'Action', width: 300, renderCell: (cellValues) => {
+                return (
+                    <div>
+                        <Button variant="outlined" onClick={() => {
+                            handleClickOpen(); handleClick(cellValues);
+                        }}>
+                            Open alert dialog
+                        </Button>
+                        <Dialog
+                            open={open}
+                            onClose={handleClose}
+                            aria-labelledby="alert-dialog-title"
+                            aria-describedby="alert-dialog-description"
+                        >
+                            <DialogTitle id="alert-dialog-title">
+                                {"Use Google's location service?"}
+                            </DialogTitle>
+                            <DialogContent>
+                                <DialogContentText id="alert-dialog-description">
+                                    Let Google help apps determine location. This means sending anonymous
+                                    location data to Google, even when no apps are running.
+                                </DialogContentText>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={handleClose}>Disagree</Button>
+                                <Button onClick={handleClose} autoFocus>
+                                    Agree
+                                </Button>
+                            </DialogActions>
+                        </Dialog>
+                    </div>
+                );
+            }
+        }
+
+    ];
+
+    return (
+        <div style={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={tableData}
+                columns={columns}
+                pageSize={5}
+                rowsPerPageOptions={[5]}
+                checkboxSelection
+                onCellClick={handleCellClick}
+                onRowClick={handleRowClick}
+            />
+        </div>
+    );
+}
+
+export default Table
